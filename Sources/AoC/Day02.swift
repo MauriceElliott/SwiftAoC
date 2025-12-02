f9f1df9852bd49e58758749ef95fbae70e08d4f1
MauriceElliott <mauricemelliott@gmail.com>
2025-12-02 20:25:34 +0000

    day2 part 2 continued

--- Sources/AoC/Day02.swift
+++ Sources/AoC/Day02.swift
@@ -1,5 +1,29 @@
 import Foundation
 
+private func findPattern(_ patternLength: Int, _ str: String) -> Bool {
+    print("[findPattern] pl: \(patternLength), str: \(str)")
+    var chunks: [String] = []
+    for i in stride(from: 0, to: str.count, by: patternLength) {
+        let startIndex = str.index(str.startIndex, offsetBy: i)
+        let endIndex = str.index(startIndex, offsetBy: min(patternLength, str.count - i))
+        chunks.append(String(str[startIndex..<endIndex]))
+    }
+    var prevStr: String? = nil
+    var patternAquired = false
+    for chunk in chunks {
+        if prevStr == nil {
+            prevStr = chunk
+        } else if chunk == prevStr {
+            prevStr = chunk
+            patternAquired = true
+        } else if chunk != prevStr {
+            patternAquired = false
+            break
+        }
+    }
+    return patternAquired
+}
+
 struct Day02 {
     static func main() {
         let c = getContents("input/day2/input.txt")!
@@ -12,17 +36,12 @@ struct Day02 {
             for i in start...end {
                 let o = String(i)
                 let sc = o.count
-                if sc % 2 == 0 {
-                    if sc == 2 && o.first == o.last {
+                print("i: \(i), sc: \(sc), o: \(o)")
+                for pl in 0...(sc / 2) {
+                    if pl == 0 { continue }              
+                    if findPattern(pl, o) {
                         total += i
-                    } else if sc == 4 && (o.prefix(2) == o.suffix(2) || Set(o).count <= 1) {
-                        total += i
-                    } else if sc == 6 && (o.prefix(3) == o.prefix(3) || o.prefix(2) == o.dropFirst(2).prefix(2) || Set(o).count <= 1) {
-                        total += i
-                    }
-                } else {
-                    if Set(o).count <= 1 {
-                        total += i
+                        break
                     }
                 }
             }
