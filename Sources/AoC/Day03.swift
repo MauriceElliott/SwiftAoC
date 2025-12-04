02b7fad4c69658c64edbf397bdd911b760cf0648
MauriceElliott <maurice.elliott@telusagcg.com>
2025-12-04 14:15:41 +0000

    give up on day 3!

--- Sources/AoC/Day03.swift
+++ Sources/AoC/Day03.swift
@@ -11,15 +11,25 @@ private func evaluate(_ s: String, _ remaining: Int) -> String {
     var highestIndex = 0
     for _ in 0..<remaining {
         print("[DEBUG] Entered For Loop, result: \(result), count: \(result.count)")
+        var searchFromIndex = 0
+        var resetSearchFromIndex = false
         if result.count > 0 {
             popped.append(highestIndex)
-            print("[DEBUG] popped: \(popped)")
+            print("[DEBUG] highestIndex \(highestIndex) sarr.count \(sArr.count - 1)")
+            if highestIndex < (sArr.count - 1) {
+                searchFromIndex = highestIndex
+                resetSearchFromIndex = true
+            } else if resetSearchFromIndex == true {
+                searchFromIndex = 0
+                resetSearchFromIndex = false
+            }
+            print("[DEBUG] popped: \(popped) sfi: \(searchFromIndex)")
         }
         highest = 0
         highestIndex = 0
         for (i, c) in sArr.enumerated() {
             let ci = Int(String(c))!
-            if popped.contains(i) {
+            if popped.contains(i) || i < searchFromIndex {
                 continue
             }
             if ci > highest {
@@ -42,11 +52,14 @@ private func evaluate(_ s: String, _ remaining: Int) -> String {
 
 struct Day03 {
     static func main() {
-        let input = getContents("input/day3/sample.txt")!
+        let input = getContents("input/day3/input.txt")!
         let l = input.split(separator: "\n")
         var total = 0
-        for r in l {
-            total += Int(evaluate(String(r), 2))!
+        for (i,r) in l.enumerated() {
+            if i == 2 {
+                break
+            }
+            total += Int(evaluate(String(r), 12))!
         }
         print(total)
     }
